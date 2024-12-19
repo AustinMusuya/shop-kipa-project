49fabc8b235b72ca47d2e7d016777138a438d899
AustinMusuya <amusuya57@gmail.com>
2024-12-19 05:49:11 +0300

    add urls for register and login

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home" ),
+    path('register/', views.SignUpView.as_view(), name="register"),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
