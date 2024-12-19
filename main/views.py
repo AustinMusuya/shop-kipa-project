b18345184c93ba8428d2f53a33307e0c8d9830fb
AustinMusuya <amusuya57@gmail.com>
2024-12-19 05:49:35 +0300

    create views for user registration

--- main/views.py
+++ main/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .forms import RegisterForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'main/base.html'
+
+class SignUpView(CreateView):
+    form_class = RegisterForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/register.html"
+
     
