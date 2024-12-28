3f0a2c287a5594147c8a420ed90683abbaf61d10
AustinMusuya <amusuya57@gmail.com>
2024-12-28 19:42:12 +0300

    create views with permissions to allow authorized users to perfomr crud operations on categories.

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from .forms import RegisterForm
+from .models import Category
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -18,3 +20,34 @@ class SignUpView(CreateView):
 
 class ProfileView(TemplateView):
     template_name = 'accounts/profile.html'    
+
+class AdminRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_staff  # Only staff can access
+
+class CategoryListView(LoginRequiredMixin, ListView):
+    model = Category
+    template_name = 'categories/category-list.html'
+    context_object_name = 'categories'
+
+class CategoryDetailView(LoginRequiredMixin, DetailView):
+    model = Category
+    template_name = 'categories/category-detail.html'
+    context_object_name = 'category'
+
+class CategoryCreateView(AdminRequiredMixin, CreateView):
+    model = Category
+    template_name = 'categories/category-form.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('category-list')
+
+class CategoryUpdateView(AdminRequiredMixin, UpdateView):
+    model = Category
+    template_name = 'categories/category-form.html'
+    fields = ['name', 'description']
+    success_url = reverse_lazy('category-list')
+
+class CategoryDeleteView(AdminRequiredMixin, DeleteView):
+    model = Category
+    template_name = 'categories/category-confirm-delete.html'
+    success_url = reverse_lazy('category-list')
