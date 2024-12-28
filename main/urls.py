948a70c35f3b0c5b9a4dff8e60cdd4a1725dada6
AustinMusuya <amusuya57@gmail.com>
2024-12-28 19:41:36 +0300

    add url paths to perform crud operations on categories

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('accounts/profile/', views.ProfileView.as_view(), name='profile'),
+
+    # path to product categories
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+    path('categories/add/', views.CategoryCreateView.as_view(), name='category-add'),
+    path('categories/<int:pk>/edit/', views.CategoryUpdateView.as_view(), name='category-edit'),
+    path('categories/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category-delete'),
 ]
