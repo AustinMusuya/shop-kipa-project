d4777c0934543c2ff58a1afb4a96e2d07543063d
AustinMusuya <amusuya57@gmail.com>
2024-12-29 18:10:09 +0300

    create orders form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,8 @@
-from django import forms
+from django import forms 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import inlineformset_factory
+from .models import Orders, OrderDetail
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +10,16 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class OrdersForm(forms.ModelForm):
+    class Meta:
+        model = Orders
+        fields = ['supplier', 'status', 'total_amount']
+
+OrderDetailFormset = inlineformset_factory(
+    Orders,
+    OrderDetail,
+    fields=['product', 'quantity_ordered', 'unit_price'],
+    extra=1,
+    can_delete=True
+)
