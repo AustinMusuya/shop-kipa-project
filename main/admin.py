15736e26a05822d7890348930148fd4ee2feb031
AustinMusuya <amusuya57@gmail.com>
2024-12-31 14:03:10 +0300

    register all models to admin panel and make migrations

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product, SaleDetail, Sales, Supplier, OrderDetail, Orders
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Supplier)
+admin.site.register(Orders)
+admin.site.register(OrderDetail)
+admin.site.register(Sales)
+admin.site.register(SaleDetail)
